4a3fb58fb0e93206fa318789796876f8c14e99ad
niki4smirn <kitos.smirnov@gmail.com>
2022-04-28 15:16:36 +0300

    Add ERROR_LOG macro

--- src/Helpers/logging.h
+++ src/Helpers/logging.h
@@ -3,14 +3,15 @@
 #include <QString>
 
 #define LOG qDebug().noquote().nospace()
-#define ASSERT(condition, message) \
+#define ERROR_LOG(msg) qFatal("%s", \
+                                (std::string(__FILE__) + ':' + \
+                                std::to_string(__LINE__) + ' ' + \
+                                std::string(msg)                 \
+                                ).c_str())
+#define ASSERT(condition, msg) \
     ((condition) ? \
     static_cast<void>(0) : \
-    static_cast<void>(LOG << __FILE__ << ':' << __LINE__ \
-                          << " Assertation failed: " << (message)));   \
-    ((condition) ? \
-    static_cast<void>(0) : \
-    static_cast<void>(std::abort()))
+    static_cast<void>(ERROR_LOG(std::string(" Assertion failed: ") + (msg))));
 
 namespace log {
 

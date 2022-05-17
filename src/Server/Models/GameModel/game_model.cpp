d0c060838cb5340178886f66490d864cbb765b97
DudkoAndrei <dudkoandrei@gmail.com>
2022-05-17 22:24:03 +0300

    Update GameModel class

--- src/Server/Models/GameModel/game_model.cpp
+++ src/Server/Models/GameModel/game_model.cpp
@@ -1,1 +1,50 @@
 #include "game_model.h"
+
+const std::unordered_map<UserId,
+                         std::shared_ptr<User>>& GameModel::GetPlayers() const {
+  return players_;
+}
+
+void GameModel::AddPlayer(std::shared_ptr<User> player) {
+  players_[player->GetId()] = std::move(player);
+}
+
+AbstractMinigame* GameModel::GetMinigameById(MinigameId id) {
+  auto type = static_cast<MinigameType>(id);
+
+  return minigames_[type];
+}
+
+void GameModel::AddPlayerToMinigame(UserId player_id,
+                                    MinigameId minigame_id) {
+  auto type = static_cast<MinigameType>(minigame_id);
+
+  available_minigames_[type].push_back(players_[player_id]);
+
+  // TODO(Andrei): remove magic number
+  if (available_minigames_[type].size() == 4) {
+    emit StartMinigame(type);
+  }
+
+  emit SendGameInfo();
+}
+
+const
+  std::unordered_map
+    <MinigameType, std::vector<std::shared_ptr<User>>>&
+      GameModel::GetAvailableMinigames() const {
+  return available_minigames_;
+}
+
+std::optional<MinigameType> GameModel::GetMinigameTypeByPlayerId(
+    UserId id) const {
+  if (minigame_by_player_id_.contains(id)) {
+    return minigame_by_player_id_.at(id);
+  }
+
+  return std::nullopt;
+}
+
+uint64_t GameModel::GetProgress() const {
+  return progress_;
+}
--- src/Server/Models/GameModel/game_model.h
+++ src/Server/Models/GameModel/game_model.h
@@ -1,13 +1,43 @@
 #pragma once
 
 #include <unordered_map>
+#include <vector>
+#include <optional>
 
-#include "src/Server/Models/Player/player.h"
+#include "src/Server/Models/User/user.h"
+#include "src/Server/Games/minigames.h"
 
-class GameModel {
+class GameModel : public QObject {
+  Q_OBJECT
+
  public:
   GameModel() = default;
 
+  const std::unordered_map<UserId, std::shared_ptr<User>>& GetPlayers() const;
+  uint64_t GetProgress() const;
+
+  void AddPlayer(std::shared_ptr<User> player);
+  void AddPlayerToMinigame(UserId player_id, MinigameId minigame_id);
+
+  AbstractMinigame* GetMinigameById(MinigameId id);
+
+  std::optional<MinigameType> GetMinigameTypeByPlayerId(UserId id) const;
+
+  const
+    std::unordered_map<MinigameType, std::vector<std::shared_ptr<User>>>&
+      GetAvailableMinigames() const;
+
+ signals:
+  void SendGameInfo();
+  void StartMinigame(MinigameType type);
+
  private:
-  std::unordered_map<PlayerId, Player> players_;
+  uint64_t progress_{0};
+
+  std::unordered_map<UserId, std::shared_ptr<User>> players_;
+
+  std::unordered_map<MinigameType, AbstractMinigame*> minigames_;
+  std::unordered_map<UserId, MinigameType> minigame_by_player_id_;
+  std::unordered_map<MinigameType, std::vector<std::shared_ptr<User>>>
+      available_minigames_;
 };

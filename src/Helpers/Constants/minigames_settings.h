67224882bd6aff8f1bebfffe4eeab12f8b7d26e1
Yahor <egorlechenko805@gmail.com>
2022-06-23 20:59:19 +0300

    Fix minigame

--- src/Helpers/Constants/minigames_settings.h
+++ src/Helpers/Constants/minigames_settings.h
@@ -3,10 +3,9 @@
 namespace hole_repair_settings {
 
 extern const int kGameDuration;
-extern const int kMaxDeviation;
+extern const double kMaxDeviation;
 extern const int kHolesNumber;
 extern const int kAdditionalPlates;
-extern const int kRelativeCoords;
 extern const int kMaxMistakes;
 
 }  // namespace hole_repair_settings
